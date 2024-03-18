0d15343322ffcd1b3d82ffac31ad6a02cbc32a5d
Dario <d.dallariva@nekte.it>
2024-03-18 10:26:08 +0100

    The most important changes include the addition of a new `Person` class in `Program.cs` and the creation of a new API controller for managing `Customer` objects in `CustomersController.cs`.
    Changes:
    
    1. In `Program.cs`, a new `Person` class was added. This class includes properties for `Name`, `Lastname`, `Age`, `Address`, `Email`, `PhoneNumber`, `Date`, `City`, `State`, and `Country`. A method `GetFullName` was also added to return the full name of a `Person` object. An array of 10 `Person` objects was created with random data for each property. The full name of the first person in the array is then retrieved using the `GetFullName` method. (See `Program.cs`)
    
    2. In `CustomersController.cs`, a new API controller was created to manage `Customer` objects. This controller includes methods for getting all customers (`GetCustomers`), getting a specific customer by ID (`GetCustomer`), updating a customer (`PutCustomer`), creating a new customer (`PostCustomer`), and deleting a customer (`DeleteCustomer`). The controller uses an instance of `AdventureWorksContext` to interact with the database. A private method `CustomerExists` was also added to check if a customer exists in the database by their ID. (See `CustomersController.cs`)

---
+++ GithubCopilotDemoApi/Controllers/CustomersController.cs
@@ -0,0 +1,96 @@
+﻿using GithubCopilotDemoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GithubCopilotDemoApi.Controllers;
+
+//generate an api controller to manage customers
+[ApiController]
+[Route("[controller]")]
+
+public class CustomersController : ControllerBase
+{
+	private readonly AdventureWorksContext _context;
+
+	public CustomersController(AdventureWorksContext context)
+	{
+		_context = context;
+	}
+
+	// GET: Customers
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+	{
+		return await _context.Customers.AsNoTracking().ToListAsync();
+	}
+
+	// GET: Customers/5
+	[HttpGet("{id}")]
+	public async Task<ActionResult<Customer>> GetCustomer(int id)
+	{
+		var customer = await _context.Customers.FindAsync(id);
+
+		return customer == null ? (ActionResult<Customer>)NotFound() : (ActionResult<Customer>)customer;
+	}
+
+	// PUT: Customers/5
+	[HttpPut("{id}")]
+	public async Task<IActionResult> PutCustomer(int id, Customer customer)
+	{
+		if (id != customer.CustomerId)
+		{
+			return BadRequest();
+		}
+
+		_context.Entry(customer).State = EntityState.Modified;
+
+		try
+		{
+			await _context.SaveChangesAsync();
+		}
+		catch (DbUpdateConcurrencyException)
+		{
+			if (!CustomerExists(id))
+			{
+				return NotFound();
+			}
+			else
+			{
+				throw;
+			}
+		}
+
+		return NoContent();
+	}
+
+	// POST: Customers
+	[HttpPost]
+	public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+	{
+		_context.Customers.Add(customer);
+		await _context.SaveChangesAsync();
+
+		return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+	}
+
+	// DELETE: Customers/5
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteCustomer(int id)
+	{
+		var customer = await _context.Customers.FindAsync(id);
+		if (customer == null)
+		{
+			return NotFound();
+		}
+
+		_context.Customers.Remove(customer);
+		await _context.SaveChangesAsync();
+
+		return NoContent();
+	}
+
+	private bool CustomerExists(int id)
+	{
+		return _context.Customers.Any(e => e.CustomerId == id);
+	}
+}
--- GithubCopilotDemoConsoleApp/Program.cs
+++ GithubCopilotDemoConsoleApp/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Generate an array of 10 people using the class Person with random data
+Person[] people = new Person[10];
+var random = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+	people[i] = new Person
+	{
+		Name = "Person " + i,
+		Age = random.Next(18, 100),
+		Address = "Address " + i,
+		Email = "person" + i + "@gmail.com",
+		PhoneNumber = "1234567890",
+		Date = DateTime.Now,
+		City = "City " + i,
+		State = "State " + i,
+		Country = "Country " + i
+	};
+}
+
+// Get the full name of the first person
+var fullName = GetFullName(people[0]);
+
+static string GetFullName(Person person)
+{
+	return person.Name + " " + person.Lastname;
+}
+
+/// <summary>
+/// Represents a person.
+/// </summary>
+public class Person
+{
+	/// <summary>
+	/// Gets or sets the name of the person.
+	/// </summary>
+	public string Name { get; set; }
+
+	/// <summary>
+	/// Gets or sets the last name of the person.
+	/// </summary>
+	public string Lastname { get; set; }
+
+	/// <summary>
+	/// Gets or sets the age of the person.
+	/// </summary>
+	public int Age { get; set; }
+
+	/// <summary>
+	/// Gets or sets the address of the person.
+	/// </summary>
+	public string Address { get; set; }
+
+	/// <summary>
+	/// Gets or sets the email of the person.
+	/// </summary>
+	public string Email { get; set; }
+
+	/// <summary>
+	/// Gets or sets the phone number of the person.
+	/// </summary>
+	public string PhoneNumber { get; set; }
+
+	/// <summary>
+	/// Gets or sets the date of the person.
+	/// </summary>
+	public DateTime Date { get; set; }
+
+	/// <summary>
+	/// Gets or sets the city of the person.
+	/// </summary>
+	public string City { get; set; }
+
+	/// <summary>
+	/// Gets or sets the state of the person.
+	/// </summary>
+	public string State { get; set; }
+
+	/// <summary>
+	/// Gets or sets the country of the person.
+	/// </summary>
+	public string Country { get; set; }
+}
